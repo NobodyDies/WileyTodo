0c5e717cba084f0cee2910d6b13dcefa506b26f4
Khamit <nobodydies.ufa@gmail.com>
2016-02-06 21:47:09 +0100

    begining. Add my bootstrap code.

---
+++ README.md
@@ -0,0 +1,3 @@
+# FrontendBootstrap
+
+php -S localhost:8000 - starts a server. All created pages available in http://localhost:8000/pages/{name of the file}
---
+++ partials/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="wrapper">
+	<div id="header">
+
+	</div>
+	<div id="nav">
+
+	</div>
+	<div id="body">
