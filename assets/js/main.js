69dc482de1668fb33f12acbbc8f469e8e095c2db
Khamit <nobodydies.ufa@gmail.com>
2016-02-07 18:36:53 +0100

    animation on add and remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
+import Lockr from "lockr";
 
 new Vue({
   el: "#app",
   data: {
-    tasks: [],
+    tasks: Lockr.get('tasks')?Lockr.get('tasks'):[],
     newTask: '',
     order: -1
+  },
+  created() {
   },
   methods: {
     addTask() {
@@ -14,16 +17,19 @@ new Vue({
         title: this.newTask,
         complited: false
       });
+      Lockr.set('tasks', this.tasks);
       this.newTask = '';
     },
     toggleComplition(task) {
       task.complited = !task.complited;
+      Lockr.set('tasks', this.tasks);
     },
     deleteTask(task) {
       var index = this.tasks.indexOf(task);
       if (index > -1) {
         this.tasks.splice(index, 1);
       }
+      Lockr.set('tasks', this.tasks);
     }
   }
 })
--- js/main.js
+++ js/main.js
@@ -5,15 +5,21 @@ var _vue = require("vue");
 
 var _vue2 = _interopRequireDefault(_vue);
 
+var _lockr = require("lockr");
+
+var _lockr2 = _interopRequireDefault(_lockr);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 new _vue2.default({
   el: "#app",
   data: {
-    tasks: [],
+    tasks: _lockr2.default.get('tasks') ? _lockr2.default.get('tasks') : [],
     newTask: '',
     order: -1
   },
+  created: function created() {},
+
   methods: {
     addTask: function addTask() {
       this.tasks.push({
@@ -21,21 +27,195 @@ new _vue2.default({
         title: this.newTask,
         complited: false
       });
+      _lockr2.default.set('tasks', this.tasks);
       this.newTask = '';
     },
     toggleComplition: function toggleComplition(task) {
       task.complited = !task.complited;
+      _lockr2.default.set('tasks', this.tasks);
     },
     deleteTask: function deleteTask(task) {
       var index = this.tasks.indexOf(task);
       if (index > -1) {
         this.tasks.splice(index, 1);
       }
+      _lockr2.default.set('tasks', this.tasks);
     }
   }
 });
 
-},{"vue":3}],2:[function(require,module,exports){
+},{"lockr":2,"vue":4}],2:[function(require,module,exports){
+(function(root, factory) {
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = factory(root, exports);
+    }
+  } else if (typeof define === 'function' && define.amd) {
+    define(['exports'], function(exports) {
+      root.Lockr = factory(root, exports);
+    });
+  } else {
+    root.Lockr = factory(root, {});
+  }
+
+}(this, function(root, Lockr) {
+  'use strict';
+
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(elt /*, from*/)
+    {
+      var len = this.length >>> 0;
+
+      var from = Number(arguments[1]) || 0;
+      from = (from < 0)
+      ? Math.ceil(from)
+      : Math.floor(from);
+      if (from < 0)
+        from += len;
+
+      for (; from < len; from++)
+      {
+        if (from in this &&
+            this[from] === elt)
+          return from;
+      }
+      return -1;
+    };
+  }
+
+  Lockr.prefix = "";
+
+  Lockr._getPrefixedKey = function(key, options) {
+    options = options || {};
+
+    if (options.noPrefix) {
+      return key;
+    } else {
+      return this.prefix + key;
+    }
+
+  };
+
+  Lockr.set = function (key, value, options) {
+    var query_key = this._getPrefixedKey(key, options);
+
+    try {
+      localStorage.setItem(query_key, JSON.stringify({"data": value}));
+    } catch (e) {
+      if (console) console.warn("Lockr didn't successfully save the '{"+ key +": "+ value +"}' pair, because the localStorage is full.");
+    }
+  };
+
+  Lockr.get = function (key, missing, options) {
+    var query_key = this._getPrefixedKey(key, options),
+        value;
+
+    try {
+      value = JSON.parse(localStorage.getItem(query_key));
+    } catch (e) {
+        try {
+            if(localStorage[query_key]) {
+                value = JSON.parse('{"data":"' + localStorage.getItem(query_key) + '"}');
+            } else{
+                value = null;
+            }
+        } catch (e) {
+            if (console) console.warn("Lockr could not load the item with key " + key);
+        }
+    }
+    if(value === null) {
+      return missing;
+    } else if (typeof value.data !== 'undefined') {
+      return value.data;
+    } else {
+      return missing;
+    }
+  };
+
+  Lockr.sadd = function(key, value, options) {
+    var query_key = this._getPrefixedKey(key, options),
+        json;
+
+    var values = Lockr.smembers(key);
+
+    if (values.indexOf(value) > -1) {
+      return null;
+    }
+
+    try {
+      values.push(value);
+      json = JSON.stringify({"data": values});
+      localStorage.setItem(query_key, json);
+    } catch (e) {
+      console.log(e);
+      if (console) console.warn("Lockr didn't successfully add the "+ value +" to "+ key +" set, because the localStorage is full.");
+    }
+  };
+
+  Lockr.smembers = function(key, options) {
+    var query_key = this._getPrefixedKey(key, options),
+        value;
+
+    try {
+      value = JSON.parse(localStorage.getItem(query_key));
+    } catch (e) {
+      value = null;
+    }
+
+    if (value === null)
+      return [];
+    else
+      return (value.data || []);
+  };
+
+  Lockr.sismember = function(key, value, options) {
+    var query_key = this._getPrefixedKey(key, options);
+
+    return Lockr.smembers(key).indexOf(value) > -1;
+  };
+
+  Lockr.getAll = function () {
+    var keys = Object.keys(localStorage);
+
+    return keys.map(function (key) {
+      return Lockr.get(key);
+    });
+  };
+
+  Lockr.srem = function(key, value, options) {
+    var query_key = this._getPrefixedKey(key, options),
+        json,
+        index;
+
+    var values = Lockr.smembers(key, value);
+
+    index = values.indexOf(value);
+
+    if (index > -1)
+      values.splice(index, 1);
+
+    json = JSON.stringify({"data": values});
+
+    try {
+      localStorage.setItem(query_key, json);
+    } catch (e) {
+      if (console) console.warn("Lockr couldn't remove the "+ value +" from the set "+ key);
+    }
+  };
+
+  Lockr.rm =  function (key) {
+    localStorage.removeItem(key);
+  };
+
+  Lockr.flush = function () {
+    localStorage.clear();
+  };
+  return Lockr;
+
+}));
+
+},{}],3:[function(require,module,exports){
 // shim for using process in browser
 
 var process = module.exports = {};
@@ -128,7 +308,7 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 (function (process,global){
 /*!
  * Vue.js v1.0.16
@@ -9723,6 +9903,6 @@ if (devtools) {
 
 module.exports = Vue;
 }).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"_process":2}]},{},[1]);
+},{"_process":3}]},{},[1]);
 
 //# sourceMappingURL=main.js.map
